f935712cbea4a25b0c70defdd3c6f096befcca38
Sergio Romero Barra <s.romerobarra@gmail.com>
2016-04-26 11:00:34 +0200

    Reset password added (Need email config).

--- idiomactivaAdmin/wsgi.py
+++ idiomactivaAdmin/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for idiomactivaAdministration project.
+WSGI config for idiomactivaAdmin project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "idiomactivaAdministration.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "idiomactivaAdmin.settings")
 
 application = get_wsgi_application()
